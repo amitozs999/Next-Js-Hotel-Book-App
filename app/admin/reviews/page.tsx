6101328995a863f3542dd713e1b0ef93ca813ef4
amitozs999 <ekas0001@gmail.com>
2023-10-17 21:54:11 +0530

    front page added for admin

---
+++ app/admin/reviews/page.tsx
@@ -0,0 +1,9 @@
+import RoomReviews from "@/components/admin/RoomReviews";
+
+export const metadata = {
+  title: "Room Reviews  ",
+};
+
+export default function AdminRoomReviewsPage() {
+  return <RoomReviews />; //room review comp
+}
